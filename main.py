3b1d1037029351840265cbfe99be7461c5e49785
Ta234502 <pammiali35@gmail.com>
2026-03-10 15:17:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import redis
 # ===== CONFIG =====
 API_ID = 13516702
 API_HASH = "bf0cc3f062841935d3d5da65134ca4cf"
-BOT_TOKEN = "8698277473:AAH5fZ35MB4JxbmrApMs5Ez9l78DRpsZqMw"
+BOT_TOKEN = "8698277473:AAEkWMwudRlP3LvfU66zIT5hIMydmBjuTC0"
 REDIS_URL = os.environ.get('REDIS_URL')
 
 OUTPUT_DIR = "pyrogram_recordings"
